273c75aa0bed1b68b84dbae0658ed19e069ca7b7
radical-edo <radical.3do@gmail.com>
2026-04-16 15:08:14 +0200

    switched to new lsp setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,19 +1,39 @@
 return {
   {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      {
-        "mason-org/mason.nvim",
-        opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
-            },
+    "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "lua_ls",
+      "ts_ls",
+      "html"
+    }
+  },
+  dependencies = {
+    {
+      "mason-org/mason.nvim",
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
           },
         },
-      }
-    }
+      },
+    },
+    'neovim/nvim-lspconfig',
   }
+  },
+  { "hrsh7th/nvim-cmp" },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function ()
+      local cmp = require("cmp_nvim_lsp")
+      local capabilities = cmp.default_capabilities()
+      vim.lsp.config("*", {
+        capabilities = capabilities
+      })
+    end
+  },
 }
